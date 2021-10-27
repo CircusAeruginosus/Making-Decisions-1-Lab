4f1272de53c2a91ce66daffcb8f2675d76c39a32
Caitlin Thompson <ctt00297@gmail.com>
2021-10-27 15:23:10 -0500

    Problem 3 Complete

--- lab.js
+++ lab.js
@@ -47,7 +47,12 @@ else {
 */
 
 //CODE HERE
-
+if (amysBirthYear===brittanisBirthYear){
+  console.log ("Amy & Brittani were born in the same year")
+}
+else {
+  console.log("Amy and Brittani were not born in the same year")
+}
 
 ////////// PROBLEM 4 //////////
 
